b679db76d338b4aac99c0f67d318f251dfba0cb4
Emad Iddin Ali <ea224pn@student.lnu.se>
2024-06-05 19:10:55 +0200

    Add project files.

---
+++ array uppgift3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Inlamning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] frågor = { "Vad är huvudstaden i Sverige?", "Vad är huvudstaden i Norge?", "Vad är huvudstaden i Danmark?", "Vad är huvudstaden i Finland?" };
+            string[] svar = { "Stockholm", "Oslo", "Köpenhamn", "Helsingfors" };
+
+            Console.WriteLine("Välj en fråga (1-4):");
+            int val = int.Parse(Console.ReadLine()) - 1;
+
+            Console.WriteLine(frågor[val]);
+            string användarSvar = Console.ReadLine();
+
+            if (användarSvar.ToLower() == svar[val].ToLower())
+            {
+                Console.WriteLine("Rätt!");
+            }
+            else
+            {
+                Console.WriteLine("Fel!");
+            }
+        }
+    }
+}
